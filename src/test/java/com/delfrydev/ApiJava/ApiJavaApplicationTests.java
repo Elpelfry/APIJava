8542015c071f306dc5b65895e85c31dcd7de5ee9
Elpelfry <delfry_paulino@ucne.edu.do>
2025-09-23 23:35:13 -0400

    Setting up the proyect

---
+++ src/test/java/com/delfrydev/ApiJava/ApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delfrydev.ApiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
